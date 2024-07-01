f1b6fd84b3b5df9dd1cfbe292a113885e2dcf33e
[Sergi Bosque Ródenas] <[sbosquero@uoc.edu]>
2024-07-01 20:22:10 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <header>
+        <h1>React Essentials</h1>
+        <p>Fundamental React components for any you will build</p>
+      </header>
+
+      <main>
+        Time to get started
+      </main>
+    </div>
+  )
+}
+
+export default App
