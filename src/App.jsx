3901d53354fb79696a85d070d934ae8451cd4d0d
[Sergi Bosque Ródenas] <[sbosquero@uoc.edu]>
2024-07-01 20:29:00 +0200

    Loading images dynamically

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
 import './App.css'
+import reactImage from './assets/react.svg';
+
+function Header(){
+  return (
+    <header>
+      <img src={reactImage} alt="reactImage" />
+      <h1>React Essentials</h1>
+      <p>Fundamental React components for any you will build</p>
+      </header>
+  )
+}
 
 function App() {
-
   return (
     <div>
-      <header>
-        <h1>React Essentials</h1>
-        <p>Fundamental React components for any you will build</p>
-      </header>
-
+      <Header></Header>
       <main>
         Time to get started
       </main>
